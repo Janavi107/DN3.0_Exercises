92f10e5c5b821e135b0dc99fe8a1be84b6eb3089
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 22:28:52 +0530

    Update Scenario2.sql

--- WEEK 2/Week 2_PLSQL/Exercise 1: Control Structures/Scenario2.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 1: Control Structures/Scenario2.sql
@@ -1,1 +1,10 @@
+--Write a PL/SQL block that iterates through all customers and sets a flag IsVIP to TRUE for those with a balance over $10,000.
+BEGIN
+  FOR customer IN (SELECT CustomerID FROM Customers WHERE Balance > 10000) LOOP
+    UPDATE Customers
+    SET IsVIP = TRUE
+    WHERE CustomerID = customer.CustomerID;
+  END LOOP;
+END;
+/
 
