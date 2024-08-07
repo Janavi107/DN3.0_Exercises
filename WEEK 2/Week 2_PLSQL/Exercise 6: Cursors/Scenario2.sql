2aeb8348bd8a4ebe4258950e34010dc4d276304a
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:33:02 +0530

    Update Scenario2.sql

--- WEEK 2/Week 2_PLSQL/Exercise 6: Cursors/Scenario2.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 6: Cursors/Scenario2.sql
@@ -1,1 +1,32 @@
+--Write a PL/SQL block using an explicit cursor ApplyAnnualFee that deducts an annual maintenance fee from the balance of all accounts.
+
+DECLARE
+  CURSOR ApplyAnnualFee IS
+    SELECT AccountID, Balance
+    FROM Accounts;
+  
+  v_AccountID Accounts.AccountID%TYPE;
+  v_Balance Accounts.Balance%TYPE;
+BEGIN
+  OPEN ApplyAnnualFee;
+  LOOP
+    FETCH ApplyAnnualFee INTO v_AccountID, v_Balance;
+    EXIT WHEN ApplyAnnualFee%NOTFOUND;
+    
+    UPDATE Accounts
+    SET Balance = Balance - 50,
+        LastModified = SYSDATE
+    WHERE AccountID = v_AccountID;
+  END LOOP;
+  CLOSE ApplyAnnualFee;
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    ROLLBACK;
+    IF ApplyAnnualFee%ISOPEN THEN
+      CLOSE ApplyAnnualFee;
+    END IF;
+END;
+/
 
