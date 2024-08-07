93c12ca27b70d952c122311d0b0a567b3c69ce49
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:23:05 +0530

    Update Scenario1.sql

--- WEEK 2/Week 2_PLSQL/Exercise 5: Triggers/Scenario1.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 5: Triggers/Scenario1.sql
@@ -1,1 +1,10 @@
+--Write a trigger UpdateCustomerLastModified that updates the LastModified column of the Customers table to the current date whenever a customer's record is updated
+
+CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
+BEFORE UPDATE ON Customers
+FOR EACH ROW
+BEGIN
+    :NEW.LastModified := SYSDATE;
+END;
+/
 
