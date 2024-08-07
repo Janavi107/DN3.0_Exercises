e44405f0dcd1c880e1c1ba2677c7f6ffaf669aaa
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:27:47 +0530

    Update Scenario3.sql

--- WEEK 2/Week 2_PLSQL/Exercise 5: Triggers/Scenario3.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 5: Triggers/Scenario3.sql
@@ -1,1 +1,25 @@
+--Write a trigger CheckTransactionRules that ensures withdrawals do not exceed the balance and deposits are positive before inserting a record into the Transactions table.
+
+CREATE OR REPLACE TRIGGER CheckTransactionRules
+BEFORE INSERT ON Transactions
+FOR EACH ROW
+DECLARE
+    v_balance NUMBER;
+BEGIN
+    IF :NEW.TransactionType = 'Withdrawal' THEN
+        SELECT Balance INTO v_balance
+        FROM Accounts
+        WHERE AccountID = :NEW.AccountID
+        FOR UPDATE;
+
+        IF v_balance < :NEW.Amount THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Error: Insufficient funds for the withdrawal.');
+        END IF;
+    ELSIF :NEW.TransactionType = 'Deposit' THEN
+        IF :NEW.Amount <= 0 THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Error: Deposit amount must be positive.');
+        END IF;
+    END IF;
+END CheckTransactionRules;
+/
 
