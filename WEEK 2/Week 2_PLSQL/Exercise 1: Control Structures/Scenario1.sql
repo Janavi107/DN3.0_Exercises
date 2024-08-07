304eee2181c08b517dd59fc73e71056004d7ee63
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 22:27:04 +0530

    Update Scenario1.sql

--- WEEK 2/Week 2_PLSQL/Exercise 1: Control Structures/Scenario1.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 1: Control Structures/Scenario1.sql
@@ -1,1 +1,15 @@
+--Write a PL/SQL block that loops through all customers, checks their age, and if they are above 60, apply a 1% discount to their current loan interest rates.
+DECLARE
+  CURSOR customer_cursor IS
+    SELECT CustomerID
+    FROM Customers
+    WHERE EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM DOB) > 60;
+BEGIN
+  FOR customer IN customer_cursor LOOP
+    UPDATE Loans
+    SET InterestRate = InterestRate * 0.99
+    WHERE CustomerID = customer.CustomerID;
+  END LOOP;
+END;
+/
 
