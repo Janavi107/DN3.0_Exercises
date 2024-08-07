8b063dd75608ac401e63b1652fd3714f441fd61b
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 22:48:52 +0530

    Update Scenario3.sql

--- WEEK 2/Week 2_PLSQL/Exercise 2: Error Handling/Scenario3.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 2: Error Handling/Scenario3.sql
@@ -1,1 +1,25 @@
+--Write a stored procedure AddNewCustomer that inserts a new customer into the Customers table.
+--If a customer with the same ID already exists, handle the exception by logging an error and preventing the insertion
+
+CREATE OR REPLACE PROCEDURE AddNewCustomer(
+    p_CustomerID IN Customers.CustomerID%TYPE,
+    p_Name IN Customers.Name%TYPE,
+    p_DOB IN Customers.DOB%TYPE,
+    p_Balance IN Customers.Balance%TYPE
+) IS
+BEGIN
+    -- Insert a new customer
+    INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+    VALUES (p_CustomerID, p_Name, p_DOB, p_Balance, SYSDATE);
+    
+    COMMIT;
+EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        DBMS_OUTPUT.PUT_LINE('Error: A customer with ID ' || p_CustomerID || ' already exists.');
+        ROLLBACK;
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+        ROLLBACK;
+END AddNewCustomer;
+/
 
