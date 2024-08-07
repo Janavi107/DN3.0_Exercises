55aaca4bff63432a60065b1a677f3c8d559394a8
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:06:00 +0530

    Update Scenario2.sql

--- WEEK 2/Week 2_PLSQL/Exercise 3: Stored Procedures/Scenario2.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 3: Stored Procedures/Scenario2.sql
@@ -1,1 +1,18 @@
+--Write a stored procedure UpdateEmployeeBonus that updates the salary of employees in a given department by adding a bonus percentage passed as a parameter.
+
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
+    p_department IN Employees.Department%TYPE,
+    p_bonus_percentage IN NUMBER
+) IS
+BEGIN
+    UPDATE Employees
+    SET Salary = Salary * (1 + p_bonus_percentage / 100),
+        LastModified = SYSDATE
+    WHERE Department = p_department;
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error updating employee bonus: ' || SQLERRM);
+END UpdateEmployeeBonus;
+/
 
