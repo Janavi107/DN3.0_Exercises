260a5102eaf03287c530df4acfce07c9f5250c84
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:20:16 +0530

    Update Scenario3.sql

--- WEEK 2/Week 2_PLSQL/Exercise 4: Functions/Scenario3.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 4: Functions/Scenario3.sql
@@ -1,1 +1,26 @@
+--Write a function HasSufficientBalance that takes an account ID and an amount as input and
+--returns a boolean indicating whether the account has at least the specified amount.
+
+CREATE OR REPLACE FUNCTION HasSufficientBalance(
+    p_account_id NUMBER,
+    p_amount NUMBER
+) RETURN BOOLEAN IS
+    v_balance NUMBER;
+BEGIN
+    SELECT Balance INTO v_balance
+    FROM Accounts
+    WHERE AccountID = p_account_id;
+
+    IF v_balance >= p_amount THEN
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN FALSE;
+    WHEN OTHERS THEN
+        RETURN FALSE;
+END HasSufficientBalance;
+/
 
