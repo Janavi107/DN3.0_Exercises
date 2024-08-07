6b1adf3a0bc66e45fc652e0bdc852c40b471ce68
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 22:30:32 +0530

    Update Scenario3.sql

--- WEEK 2/Week 2_PLSQL/Exercise 1: Control Structures/Scenario3.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 1: Control Structures/Scenario3.sql
@@ -1,1 +1,8 @@
+--Write a PL/SQL block that fetches all loans due in the next 30 days and prints a reminder message for each customer.
+BEGIN
+  FOR loan IN (SELECT LoanID, CustomerID FROM Loans WHERE EndDate <= SYSDATE + 30) LOOP
+    DBMS_OUTPUT.PUT_LINE('Reminder: Loan ID ' || loan.LoanID || ' is due within 30 days for customer ' || loan.CustomerID);
+  END LOOP;
+END;
+/
 
