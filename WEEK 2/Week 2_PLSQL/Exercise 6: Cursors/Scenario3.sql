0e33af3a2980097983470732ddce4df986db7a9c
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:33:51 +0530

    Update Scenario3.sql

--- WEEK 2/Week 2_PLSQL/Exercise 6: Cursors/Scenario3.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 6: Cursors/Scenario3.sql
@@ -1,1 +1,31 @@
+--Write a PL/SQL block using an explicit cursor UpdateLoanInterestRates that fetches all loans and updates their interest rates based on the new policy.
+
+DECLARE
+  CURSOR UpdateLoanInterestRates IS
+    SELECT LoanID, InterestRate
+    FROM Loans;
+  
+  v_LoanID Loans.LoanID%TYPE;
+  v_InterestRate Loans.InterestRate%TYPE;
+BEGIN
+  OPEN UpdateLoanInterestRates;
+  LOOP
+    FETCH UpdateLoanInterestRates INTO v_LoanID, v_InterestRate;
+    EXIT WHEN UpdateLoanInterestRates%NOTFOUND;
+    
+    UPDATE Loans
+    SET InterestRate = v_InterestRate * 1.05
+    WHERE LoanID = v_LoanID;
+  END LOOP;
+  CLOSE UpdateLoanInterestRates;
+  COMMIT;
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    ROLLBACK;
+    IF UpdateLoanInterestRates%ISOPEN THEN
+      CLOSE UpdateLoanInterestRates;
+    END IF;
+END;
+/
 
