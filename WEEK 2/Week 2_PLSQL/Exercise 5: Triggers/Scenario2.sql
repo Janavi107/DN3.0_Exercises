713d01eaf98df47c8cdea80bdaeedcaa386341c1
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:25:28 +0530

    Update Scenario2.sql

--- WEEK 2/Week 2_PLSQL/Exercise 5: Triggers/Scenario2.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 5: Triggers/Scenario2.sql
@@ -1,1 +1,11 @@
+--Write a trigger LogTransaction that inserts a record into an AuditLog table whenever a transaction is inserted into the Transactions table.
+
+CREATE OR REPLACE TRIGGER LogTransaction
+AFTER INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+    INSERT INTO AuditLog (AuditID, TransactionID, AccountID, TransactionDate, Amount, TransactionType, LogDate)
+    VALUES (AuditLog_seq.NEXTVAL, :NEW.TransactionID, :NEW.AccountID, :NEW.TransactionDate, :NEW.Amount, :NEW.TransactionType, SYSDATE);
+END LogTransaction;
+/
 
