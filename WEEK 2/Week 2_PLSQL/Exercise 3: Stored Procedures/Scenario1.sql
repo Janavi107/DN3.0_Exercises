d3a85569eed5854fa501925612440bba95c97b54
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:03:10 +0530

    Update Scenario1.sql

--- WEEK 2/Week 2_PLSQL/Exercise 3: Stored Procedures/Scenario1.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 3: Stored Procedures/Scenario1.sql
@@ -1,1 +1,17 @@
+--Write a stored procedure ProcessMonthlyInterest that calculates and 
+--updates the balance of all savings accounts by applying an interest rate of 1% to the current balance.
+
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
+BEGIN
+    UPDATE Accounts
+    SET Balance = Balance * 1.01,
+        LastModified = SYSDATE
+    WHERE AccountType = 'Savings';
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: An unexpected error occurred while processing monthly interest.');
+END ProcessMonthlyInterest;
+/
 
