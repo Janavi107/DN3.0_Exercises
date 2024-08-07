bad2f4de2e970901fdd693d028fa63b145d5c998
Janavi S <117751805+Janavi107@users.noreply.github.com>
2024-08-07 23:16:39 +0530

    Update Scenario1.sql

--- WEEK 2/Week 2_PLSQL/Exercise 4: Functions/Scenario1.sql
+++ WEEK 2/Week 2_PLSQL/Exercise 4: Functions/Scenario1.sql
@@ -1,1 +1,15 @@
+--Write a function CalculateAge that takes a customer's date of birth as input and returns their age in years.
+
+CREATE OR REPLACE FUNCTION CalculateAge (
+    p_dob DATE
+) RETURN NUMBER IS
+    v_age NUMBER;
+BEGIN
+    v_age := TRUNC(MONTHS_BETWEEN(SYSDATE, p_dob) / 12);
+    RETURN v_age;
+EXCEPTION
+    WHEN OTHERS THEN
+        RETURN NULL;
+END CalculateAge;
+/
 
